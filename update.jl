da6a411c3b01422a4137155ce09da073c61df214
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2018-07-10 17:43:05 -0400

    Now update.jl reads header and footer from external files

--- update.jl
+++ update.jl
@@ -1,26 +1,5 @@
-header = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <title>TensorNetwork</title>
-    <meta http-equiv="content-language" content="en" />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
-    <script type="text/x-mathjax-config">
-      MathJax.Hub.Config({tex2jax: {inlineMath: [['\$','\$'] ]}});
-    </script>
-    <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML">
-    </script>
-</head>
-
-<body>
-"""
-
-footer = """
-</body>
-</html>
-"""
+header_prenav = open("header_prenav") do file readstring(file) end
+footer = open("footer") do file readstring(file) end
 
 idir = "src"
 odir = "pages"
@@ -40,7 +19,7 @@ for (root,dirs,files) in walkdir(idir)
     ofname = curro*"/"*base*".html"
     html = Base.Markdown.html(Markdown.parse_file(ifname))
     of = open(ofname,"w")
-    print(of,header)
+    print(of,header_prenav)
     print(of,html)
     print(of,footer)
     close(of)
