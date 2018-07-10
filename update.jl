2eed28b5a1b93dfa732f9b65b2fdab0f0a1bed8f
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2018-07-10 17:34:05 -0400

    Updated page header

--- update.jl
+++ update.jl
@@ -10,7 +10,7 @@ header = """
     <script type="text/x-mathjax-config">
       MathJax.Hub.Config({tex2jax: {inlineMath: [['\$','\$'] ]}});
     </script>
-    <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML">
+    <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML">
     </script>
 </head>
 
