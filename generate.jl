f0f68fafd32b1dd6f6095cde2baa452d10528ec2
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2018-08-13 15:50:20 -0400

    Generator now reports any missing or incorrect wiki links

--- generate.jl
+++ generate.jl
@@ -92,9 +92,10 @@ end
 #
 # Process wiki-style links
 # 
-function processWikiLinks(html::String)
+function processWikiLinks(html::String,ifname::String)
   link_re = r"\[\[(.+?)\|(.*?)\]\]"
   sub_re = r"(.*?)(#.*)"
+  missing_links = String[]
   res = ""
   pos = 1
   match = false
@@ -102,19 +103,29 @@ function processWikiLinks(html::String)
     match = true
     res *= html[pos:m.offset-1]
     target = convert(String,m.captures[2])
+
+    #If link has a "#" in it, split into target and sublink
     sublink = ""
     for sm in eachmatch(sub_re,target)
       target = convert(String,sm.captures[1])
       sublink = convert(String,sm.captures[2])
     end
+
     if isdir("src/"*target)
       res *= "["*m.captures[1]*"](/"*target*"/index.html$sublink)"
     elseif isfile("src/"*target*".md")
       res *= "["*m.captures[1]*"](/"*target*".html$sublink)"
     else
       res *= "["*m.captures[1]*"](unknown_file)"
+      push!(missing_links,target)
     end
     pos = m.offset+length(m.match)
+  end
+  if length(missing_links) > 0
+    println("Missing/Incorrect Wiki Links in File $ifname:")
+    for link in missing_links
+      println("  $link")
+    end
   end
   if !match 
     return html 
@@ -273,7 +284,7 @@ for (root,dirs,files) in walkdir(idir)
       #generateTOC(mdstring) && println("Found TOC in $base")
       mdstring = generateTOC(mdstring,has_refs)
       (mdstring,mjlist) = processMathJax(mdstring)
-      mdstring = processWikiLinks(mdstring)
+      mdstring = processWikiLinks(mdstring,ifname)
 
       (mdstring,citenums) = processCitations(mdstring)
       btfile = curri*"/"*base*".bib"
